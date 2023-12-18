b58be72625cbcf1795ea8bf06edb28b3b7fb3f3f
TheerapatPooh <152172535+TheerapatPooh@users.noreply.github.com>
2023-12-18 19:45:16 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+    <body>
+        <h1>This is php <?php echo 'Hello World!'; ?></h1>
+        <div>
+            <p>1</p>
+            <?php
+            $var_a = 1;
+            $var_a = "1$var_a"; // 1 1
+            print_r($var_a);
+            echo "<br>" ;
+            echo $var_a . "2"; //12,3
+            $arry = array();
+            $arry = [];
+            $arry[] = 1;
+            $arry[] = 2;
+            $arry[0] = 3;
+            print_r($arry);
+            // count
+            //sizeof
+            for($i = 0;$i < sizeof($arry);$i++)
+            {
+                echo "<h1>my h1</h1>";
+            ?>
+            <p>2</p>
+            <?php
+            
+            ?>
+            }
+        </div>
+    </body>
+</html>
